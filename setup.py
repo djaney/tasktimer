fc85e857e9f8aae8e1cbd651336b0315276033bd
dmabelin <d.mabelin@arcanys.com>
2021-09-14 14:58:39 +0800

    updates

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='tasktimer',
     version='0.0.1',
-    packages=['tasktimer'],
+    packages=find_packages(),
     url='',
     license='',
     author='thedjaney',
